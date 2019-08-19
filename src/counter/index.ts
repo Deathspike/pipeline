2bb9dda505a193d77a6b3f5b037020ab01452cf9
Roel van Uden <roel.vanuden@orcagroup.com>
2019-08-19 14:42:43 +0200

    Normalize language access for multi-area use.

--- src/counter/index.ts
+++ src/counter/index.ts
@@ -1,5 +1,4 @@
 export * from '../shared';
-export * from './language';
 export * from './controllers/MainController';
 export * from './viewModels/MainViewModel';
 export * from './views/MainView';
--- src/counter/views/MainView.tsx
+++ src/counter/views/MainView.tsx
@@ -2,6 +2,7 @@ import * as app from '..';
 import * as mobxReact from 'mobx-react';
 import * as mui from '@material-ui/core';
 import * as React from 'react';
+import {language} from '../language';
 
 @mobxReact.observer
 export class MainView extends React.Component<{vm: app.MainViewModel}> {
@@ -10,17 +11,17 @@ export class MainView extends React.Component<{vm: app.MainViewModel}> {
       <mui.Grid>
         <mui.Paper style={styles.textContainer}>
           <mui.Typography>
-            {app.language.displayText} {this.props.vm.value}
+            {language.displayText} {this.props.vm.value}
           </mui.Typography>
         </mui.Paper>
         <mui.Grid style={styles.buttonContainer}>
           <mui.Button color="primary" variant="contained" style={styles.button} onClick={() => this.props.vm.increment()}>
             <app.icons.Add style={styles.buttonIcon} />
-            {app.language.incrementButton}
+            {language.incrementButton}
           </mui.Button>
           <mui.Button color="primary" variant="contained" style={styles.button} onClick={() => this.props.vm.decrement()}>
             <app.icons.Remove style={styles.buttonIcon} />
-            {app.language.decrementButton}
+            {language.decrementButton}
           </mui.Button>
         </mui.Grid>
       </mui.Grid>
