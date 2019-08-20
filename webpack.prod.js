4056c77da97a4008a33a15c61789543b644b1c77
Roel van Uden <roel.vanuden@orcagroup.com>
2019-08-20 16:55:45 +0200

    Update dependencies

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,8 +1,6 @@
-const common = require('./webpack.common.js');
-const merge = require('webpack-merge');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-
-module.exports = merge(common, {
-  plugins: [new UglifyJSPlugin({sourceMap: true, uglifyOptions: {output: {comments: false}}})],
+module.exports = {
+  entry: './dist',
+  output: {filename: 'app.min.js', path: `${__dirname}/public`},
+  performance: {hints: false},
   mode: 'production'
-});
+};
