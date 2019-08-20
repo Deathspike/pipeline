06121e31d5e6e1e0335603009e190dc0fbc6216d
Deathspike <meister.deathspike@outlook.com>
2019-08-20 16:55:45 +0200

    Update dependencies

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,8 +1,8 @@
-const common = require('./webpack.common.js');
-const merge = require('webpack-merge');
-
-module.exports = merge(common, {
+module.exports = {
   devtool: 'eval',
   devServer: {contentBase: __dirname + '/public', host: '0.0.0.0', port: 3000},
+  entry: './dist',
+  output: {filename: 'app.min.js', path: `${__dirname}/public`},
+  performance: {hints: false},
   mode: 'development'
-});
+};
