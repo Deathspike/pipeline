76c1419ba4bba01fa8399fb7143c80cc174b3123
Deathspike <meister.deathspike@outlook.com>
2019-09-05 16:06:50 +0200

    Remove mobx action enforcement

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
 import * as areas from './areas';
-import * as mobx from 'mobx';
 import * as mobxReact from 'mobx-react';
 import * as mui from '@material-ui/core';
 import * as React from 'react';
@@ -36,7 +35,6 @@ if (window.oni) {
 
 (function() {
   areas.shared.core.screen.open(Root);
-  mobx.configure({enforceActions: 'observed'});
   ReactDOM.render(<App />, document.getElementById('container'));
   setTimeout(() => window.oni && window.oni.sendAsync('shell.hideSplashScreen'), 1000);
 })();
