3f9b6e8af054bb36d8d359e795f466f0b71f903c
Roel van Uden <r.h.g.v.uden@outlook.com>
2019-09-30 16:50:59 +0200

    Remove obsolete RouteManager

--- src/shared/core.ts
+++ src/shared/core.ts
@@ -1,9 +1,7 @@
 import {DialogManager} from './managers/DialogManager';
-import {RouteManager} from './managers/RouteManager';
 import {ScreenManager} from './managers/ScreenManager';
 
 export const core = {
   dialog: new DialogManager(),
-  route: new RouteManager(),
   screen: new ScreenManager()
 };
