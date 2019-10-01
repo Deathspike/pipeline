5edea27fee905caa61656948be081f7c9a7704fd
Roel van Uden <roel.vanuden@orcagroup.com>
2019-10-01 08:28:53 +0200

    Initialize the core lazily

--- src/shared/core.ts
+++ src/shared/core.ts
@@ -1,7 +1,17 @@
-import {DialogManager} from './managers/DialogManager';
-import {ScreenManager} from './managers/ScreenManager';
+import * as app from '.';
+let cacheDialog: app.DialogManager;
+let cacheScreen: app.ScreenManager;
 
 export const core = {
-  dialog: new DialogManager(),
-  screen: new ScreenManager()
+  get dialog() {
+    if (cacheDialog) return cacheDialog;
+    cacheDialog = new app.DialogManager();
+    return cacheDialog;
+  },
+
+  get screen() {
+    if (cacheScreen) return cacheScreen;
+    cacheScreen = new app.ScreenManager();
+    return cacheScreen;
+  }
 };
--- src/shared/index.ts
+++ src/shared/index.ts
@@ -5,5 +5,7 @@ export * from './common/styles';
 export * from './common/theme';
 export * from './components/HeaderComponent';
 export * from './components/LoadingComponent';
+export * from './managers/DialogManager';
+export * from './managers/ScreenManager';
 export * from './views/DialogManagerView';
 export * from './views/ScreenManagerView';
